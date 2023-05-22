deec8cead96fa2d40d83a7f9ee59afcdb2c5680d
zodman <zodman@gmail.com>
2023-05-22 14:20:27 -0400

    running big query

--- models/paid_ads.sql
+++ models/paid_ads.sql
@@ -1,43 +1,29 @@
-{#TODO: change when big query changes #}
-{% set postgres_map_fields = dict(string='varchar', int64='integer', date='date') %}
 {% set remap_fields = var('remap_fields') %}
-
 
 WITH paid_ads  AS (
 
-    {% for channel in var('sources_list') %}
-
-        SELECT
-            {% for column, type_ in get_mcdm_fields() %}
-
-                {%if column in remap_fields[channel] %}
-
+    {% for channel in var('sources_list') -%}
+         SELECT
+            {% for column, type_ in get_mcdm_fields() -%}
+                {% if column in remap_fields[channel] -%}
                     {{ remap_fields[channel][column] }}
-
-                {% elif column in get_all_columns(channel) %}
-
-                    CAST( {{column}} as {{postgres_map_fields.get(type_)}} ) 
-
-                {% else %}
-                    {%if type_ == 'string' %}
+                {% elif column in get_all_columns(channel) -%}
+                    CAST( {{column}} AS {{type_}} ) 
+                {% else -%}
+                    {% if type_ == 'string' -%}
                         ''
-                    {%elif type_ == 'int64' %}
+                    {% elif type_ == 'int64' -%}
                         0
                     {% endif %}
-                {% endif %}
-
-                as {{column}}
-
-                {% if not loop.last %},{% endif %}
-
+                {% endif %} AS {{column}} {% if not loop.last %},{% endif %}
             {% endfor %}
         FROM 
             {{ source('source_data',channel) }}
 
         {% if not loop.last %}
-        UNION
+        UNION ALL
         {% endif %}
-    {% endfor %}
+    {%- endfor %}
 )
 
 SELECT *  FROM paid_ads
